28228db6d70937255495eaaf42c00b8539e1cf7e
Hasebul <hasebulhassan21@gmail.com>
2025-04-19 01:58:43 +0600

    refactored

--- quick-chat-client/src/app/chat-room/chat-room.component.ts
+++ quick-chat-client/src/app/chat-room/chat-room.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, Input } from '@angular/core';
+import { ChangeDetectorRef, Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { UserStatusComponent } from '../user-status/user-status.component';
 import { ChatBoxComponent } from '../chat-box/chat-box.component';
 import { FormsModule } from '@angular/forms';
@@ -8,6 +8,8 @@ import { RouterModule } from '@angular/router';
 import { HomeComponent } from '../home/home.component';
 import SockJS from 'sockjs-client';
 import { Stomp } from '@stomp/stompjs';
+import { StompService } from '../service/stomp.service';
+import { AuthService } from '../service/auth-service';
 
 @Component({
   selector: 'app-chat-room',
@@ -16,93 +18,60 @@ import { Stomp } from '@stomp/stompjs';
   templateUrl: './chat-room.component.html',
   styleUrl: './chat-room.component.scss'
 })
-export class ChatRoomComponent {
+export class ChatRoomComponent implements OnChanges, OnInit {
   private stompClient: any | undefined;
   isSelected = false;
   userName: string | undefined;
   userEmail: string | undefined;
   activeUsers: any[] = [];
-  loginUser: any | undefined = {};
+  loginUser: any | undefined;
   @Input() selectedUser: any;
   login = false;
+  isConnected = false;
 
+  constructor(private cdr: ChangeDetectorRef, 
+    private stompService : StompService, private authService: AuthService) {
+     
+  }
 
+  ngOnInit(): void {
+    console.log("Chat Room Component Initialized");
+    this.loginUser = this.authService.getLoggedInUser();
+    this.connectSocket();
+  }
 
-
-  constructor(private cdr: ChangeDetectorRef) {
-    // this.connectSocket();
-    this.loginUser = {
-      id: 1,
-      userName: 'Alice Smith',
-      userEmail: 'alice@example.com'
-    };
-
-    this.activeUsers = [
-      {
-        id: 2,
-        userName: 'Bob Johnson',
-        userEmail: 'bob@example.com'
-      },
-      {
-        id: 3,
-        userName: 'Charlie Rose',
-        userEmail: 'charlie@example.com'
-      },
-      {
-        id: 4,
-        userName: 'Diana Prince',
-        userEmail: 'diana@example.com'
-      }
-    ];
+  ngOnChanges(changes: SimpleChanges): void {
+     console.log("Chat Room Component Changes Detected", changes);
   }
 
   connectSocket() {
+    if (this.stompClient && this.stompClient.connected) {
+      console.log("STOMP client is already connected.");
+      return;
+    }
     const socket = new SockJS('http://localhost:8080/ws');
     this.stompClient = Stomp.over(socket);
 
     this.stompClient.connect({}, () => {
-      console.log("Connected as " + this.userName);
+      console.log("Connected as " + this.loginUser.userName);
       this.isSelected = true;
-      this.stompClient.send('/app/addUser', { receipt: 'message-receipt' },
-        JSON.stringify({
-          id: this.loginUser.id,
-          userName: this.loginUser.userName,
-          userEmail: this.loginUser.userEmail,
-        }
-        ));
-
-      this.stompClient.subscribe(`/topic/public`, response => {
-        const tmp = JSON.parse(response.body);
-        if (this.loginUser.userEmail === tmp.userEmail)
-          this.loginUser = JSON.parse(response.body)
-      });
-
+      this.isConnected = true;
       this.stompClient.subscribe(`/topic/public/activeUsers`, response => {
-        this.activeUsers = JSON.parse(response.body)
+        console.log("Active users update:", response.body);
+        this.activeUsers = [...JSON.parse(response.body)];
       });
 
+      this.stompClient.send('/app/chat/join', {}, JSON.stringify({
+        id: this.loginUser.id,
+        userName: this.loginUser.userName,
+        userEmail: this.loginUser.userEmail
+      }));
     }, (error) => {
       console.log(error);
     });
   };
 
-  disConnectSocket() {
-    this.isSelected = false;
-    this.userEmail = '';
-    this.userName = '';
-    this.loginUser = undefined;
-  }
-
   onUserChangeEvent(user: any) {
     this.selectedUser = user;
-  }
-
-  setloginUser(user: any) {
-    this.loginUser = user;
-    this.connectSocket();
-  }
-
-  setLoginUserName() {
-
   }
 }
